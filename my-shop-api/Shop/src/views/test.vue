def06d4b22131b2e62cee71f58c6b61cd75f7455
Mopeko <maxime.brault@epitech.digital>
2023-11-15 17:15:40 +0100

    api

--- my-shop-api/Shop/src/views/test.vue
+++ my-shop-api/Shop/src/views/test.vue
@@ -2,6 +2,8 @@
                  <section v-if="getStatus =='done'">
                       <article v-for="product in getProduct.data['hydra:member']"  :key="product.id">
             <h1>{{product.name}}</h1>
+            <h1>{{product.description}}</h1>
+            <h1>{{product.price + "€"}}</h1>
           </article></section>
                     <section v-else>...Loading</section>   
 </template>
@@ -22,5 +24,14 @@ export default{
     ...mapState(useShopdatabase, ["getProduct","getStatus"]),
   },
 }
+//created() {
+  //const article = { title: "Vue POST Request Example" };
+  //const headers = { 
+   // "Authorization": "Bearer my-token",
+   // "My-Custom-Header": "foobar"
+  //};
+  //axios.post("https://reqres.in/api/articles", article, { headers })
+   // .then(response => this.articleId = response.data.id);
+//}
 </script>
 
